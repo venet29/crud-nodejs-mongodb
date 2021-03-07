88a84ebe51e51e4579ee143d051b818dce0599e3
venet <resd@hotmail.com>
2021-03-07 16:11:40 -0300

    se agregaron varibles locales

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict'
 
 var mongoose = require('mongoose');
+const config = require("./config");
 var app = require('./app');
 var port = 3700
 
 mongoose.Promise= global.Promise;
-mongoose.connect('mongodb://localhost:27017/portafolio')
+mongoose.connect('mongodb://'+ config.MONGODB_HOST+':27017/'+config.MONGODB_DATABASE)
         .then(
             ()=> {
                 console.log('conenccion a base de datos establecida');
